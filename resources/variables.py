42184619db0752000f41abe819eb4fa1dd2a3f8e
Piia Loukeinen <p.loukeinen@gmail.com>
2024-04-13 12:46:58 +0300

    new variables and keywords

--- resources/variables.py
+++ resources/variables.py
@@ -3,7 +3,25 @@ BASE_URL = 'https://api.digitransit.fi'
 UI_URL = 'https://hsl.fi'
 BROWSER = 'chromium'
 
+ROUTE_I_SHORT_NAME = 'I'
+ROUTE_I_GTFSID = 'HSL:3001I'
+
+# UI texts
+
+HSL_TITLE = 'Reittiopas, liput ja hinnat, asiakaspalvelu | HSL.fi'
+ALERT_TEXT = 'Pääradan ja kehäradan itäosan junaliikenteeseen huomattavia muutoksia 2.4.2024 alkaen'
+
 # Locators
 
 TRAFFIC_NOW_LINK = '//a[@title="Liikenne nyt"]'
 ACCCEPT_COOKIES_BTN = '//*[@id="coiConsentBannerBase"]/div/div/button[contains(text(), "Hyväksy kaikki")]'
+TRAFFIC_PAGE_HEADER = '//*[@id="main"]/div/div/div/div/h1[text()="Liikenne"]'
+SEARCH_ROUTE_INPUT = 'id=stop-route-station'
+DISRUPTION_ELEMENT = '//*/div[contains(text(), "+' + ALERT_TEXT + '")]'
+
+
+
+
+# Query
+
+DISRUPTIONS_DATA_QUERY = '{alerts (route: ["${ROUTE_I_GTFSID}"] severityLevel: [WARNING]){alertDescriptionText}}'
--- tests/tests.robot
+++ tests/tests.robot
@@ -9,5 +9,6 @@ Suite Teardown    Suite Teardown Keywords
 First Case
     [Documentation]    todo
     Given I Open HSL Web Page
-    When I Get Disruptions Data From API
+    When I Get Disruptions Data From API For Route I
     Then I Navigate To Traffic Page
+    And I Read Current Traffic Info For Route I
